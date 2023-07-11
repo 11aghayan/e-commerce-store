413742f5f0d4c88bf031cf6e7190ea09308fcbae
11aghayan <11.aghayan.99@gmail.com>
2023-07-12 03:22:33 +0400

    final

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,29 @@
+import getBillboard from "@/actioins/get-billboard";
+import getProducts from "@/actioins/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+
+  const products = await getProducts({ isFeatured:true });
+  const billboard = await getBillboard('f69d6168-40f6-492f-9e3c-8d53d2200391');
+  
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList
+            title="Featured Products"
+            items={products}
+          />
+        </div>
+      </div>
+    </Container>
+  )
+}
+
+export default HomePage;
